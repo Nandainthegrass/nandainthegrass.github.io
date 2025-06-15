bd29273401a0822bc72091d1d30025fed6723bba
Nandainthegrass <postnanda200@gmail.com>
2025-06-15 17:03:12 +0530

    re-work

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./index.css";
+import { gsap } from "gsap";
+import { NavBar } from "./components/NavBar";
 
-
-export default function App(){
-  return <>
-    <div>hello</div>
-  </>
+export default function App() {
+  return (
+    <>
+      <NavBar/>
+      <div className="container">
+        <h1 className="screen-text" id="screen-text-h1">
+          Simplicity is essential
+        </h1>
+        <p className="screen-text" id="screen-text-p">
+          ...but sometimes, it just gotta be beautiful.
+        </p>
+      </div>
+    </>
+  );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './NavBar.css';
+
+export function NavBar () {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+      </ul>
+    </nav>
+  )
+}
